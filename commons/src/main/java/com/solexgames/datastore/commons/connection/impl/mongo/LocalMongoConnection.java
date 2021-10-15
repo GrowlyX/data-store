428bebf89df1ed103d0180eb99c897934c5aefe0
GrowlyX <rtaofficialtechie@Gmail.com>
2021-10-15 19:35:05 -0400

    Fix some stuff

--- commons/src/main/java/com/solexgames/datastore/commons/connection/impl/mongo/LocalMongoConnection.java
+++ commons/src/main/java/com/solexgames/datastore/commons/connection/impl/mongo/LocalMongoConnection.java
@@ -2,17 +2,15 @@ package com.solexgames.datastore.commons.connection.impl.mongo;
 
 import com.mongodb.MongoClient;
 import com.solexgames.datastore.commons.connection.impl.MongoConnection;
+import lombok.Getter;
 
 /**
  * @author GrowlyX
  * @since 9/8/2021
  */
+@Getter
 public class LocalMongoConnection extends MongoConnection {
-    
+
     private final MongoClient client = new MongoClient();
 
-    @Override
-    public MongoClient getClient() {
-        return this.client;
-    }
 }
--- commons/src/main/java/com/solexgames/datastore/commons/connection/impl/mongo/UriMongoConnection.java
+++ commons/src/main/java/com/solexgames/datastore/commons/connection/impl/mongo/UriMongoConnection.java
@@ -3,27 +3,21 @@ package com.solexgames.datastore.commons.connection.impl.mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.solexgames.datastore.commons.connection.impl.MongoConnection;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
  * @author GrowlyX
  * @since 9/8/2021
  */
+@Getter
 @RequiredArgsConstructor
 public class UriMongoConnection extends MongoConnection {
 
     private final String uri;
-    
-    private MongoClient client;
-    
-    {
-        this.client = new MongoClient(
-                new MongoClientURI(this.uri)
-        );
-    }
 
-    @Override
-    public MongoClient getClient() {
-        return this.client
-    }
+    private final MongoClient client = new MongoClient(
+            new MongoClientURI(this.uri)
+    );
+
 }
